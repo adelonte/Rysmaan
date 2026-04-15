422c24b943b522849dc609de4f13ee9c0e6d2f15
Sina <adelonte@gmail.com>
2026-04-14 18:19:25 -0600

    Adjust footer layout in AppFooter component by reducing padding and logo size for improved visual consistency. Update copyright year to 2026.

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,14 +5,14 @@ const links = [];
 <template>
   <footer class="bg-white dark:bg-gray-900 border-t border-gray-100 dark:border-gray-800">
     <div class="container px-4 md:px-8 xl:px-16 sm:mx-auto">
-      <div class="py-8 flex flex-col-reverse md:flex-row items-center justify-between gap-6">
+      <div class="py-4 flex flex-col-reverse md:flex-row items-center justify-between gap-4">
         <div class="text-sm text-gray-500 dark:text-gray-400">
           &copy; 2026 Rysmaan. All rights reserved.
         </div>
         
         <NuxtLink to="/" class="flex items-center gap-2">
-          <img src="/logo.png" alt="Rysmaan Logo" class="h-28 w-auto dark:hidden" />
-          <img src="/logo.png" alt="Rysmaan Logo" class="hidden dark:block h-28 w-auto invert brightness-200" />
+          <img src="/logo.png" alt="Rysmaan Logo" class="h-20 w-auto dark:hidden" />
+          <img src="/logo.png" alt="Rysmaan Logo" class="hidden dark:block h-20 w-auto invert brightness-200" />
         </NuxtLink>
       </div>
     </div>
